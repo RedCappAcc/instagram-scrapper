d4d9768c82867762cb93ba2b066a5468ae43d4bb
Temirlan Serikov <temirlan-s@auto.kz>
2023-11-02 01:24:43 +0600

    Feature: added puppeteer

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-type Test = {
-  value: string
-}
+import puppeteer from "puppeteer"
+;(async () => {
+  const browser = await puppeteer.launch({ headless: "new" })
+  const page = await browser.newPage()
+  await page.goto("https://google.com")
 
-const a: Test = {
-  value: "2",
-}
+  // const element = await page.waitForSelector('input[name="btnI"]')
+  // await element?.tap()
+  await page.screenshot({ path: "news.png", fullPage: true })
 
-console.log(a)
+  await browser.close()
+})()
